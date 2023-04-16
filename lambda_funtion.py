ad67d2fccfe104793fee6475430221a93af033a5
森田大二郎 <mority@MacBook-Pro.local>
2023-04-16 12:18:56 +0900

    added exception

--- lambda_funtion.py
+++ lambda_funtion.py
@@ -1,4 +1,5 @@
 import boto3
+from botocore.exceptions import ClientError
 import gzip
 import os
 from datetime import datetime
@@ -10,9 +11,11 @@ logs = boto3.client("logs")
 
 
 def lambda_handler(event, context):
+    # source
     bucket = event["Records"][0]["s3"]["bucket"]["name"]
     key = unquote_plus(event["Records"][0]["s3"]["object"]["key"])
 
+    # destination
     log_group = "/test/log"
     log_stream = os.path.basename(key)
     try:
@@ -22,7 +25,15 @@ def lambda_handler(event, context):
         pass
 
     with BytesIO() as data:
-        s3.download_fileobj(bucket, key, data)
+        try:
+            s3.download_fileobj(bucket, key, data)
+        except ClientError as e:
+            error_code = e.response['Error']['Code']
+            print(f"An error occurred (ClientError - {error_code}): {str(e)}")
+            if error_code == 'SlowDown':
+                # if SlowDown is occurred, raising exception for backing off and retry
+                raise e
+
         data.seek(0)
         with gzip.GzipFile(fileobj=data) as f:
             for line in f:
@@ -40,4 +51,3 @@ def lambda_handler(event, context):
                         }
                     ]
                 )
-                print("end")
